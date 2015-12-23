42055a63445f40e4dc8972427fe66872e25341f3
Vadym Markov <markov.vadym@gmail.com>
2015-12-23 18:58:27 +0100

    Theme Equatable tests

--- JukeboxTests/Specs/Data/ThemeSpec.swift
+++ JukeboxTests/Specs/Data/ThemeSpec.swift
@@ -25,6 +25,29 @@ class ThemeSpec: QuickSpec {
           expect(theme.bundleName).to(equal("\(Theme.bundlePrefix)-\(theme.name)"))
         }
       }
+
+      describe("==") {
+        it("is true when themes are equal") {
+          let theme1 = Theme(
+            name: "Amazing",
+            audioFormat: "aiff")
+
+          expect(theme).to(equal(theme1))
+        }
+
+        it("is false when themes are not equal") {
+          let theme1 = Theme(
+            name: "Amazing",
+            audioFormat: "wav")
+
+          let theme2 = Theme(
+            name: "Unknown",
+            audioFormat: "aiff")
+
+          expect(theme).toNot(equal(theme1))
+          expect(theme).toNot(equal(theme2))
+        }
+      }
     }
   }
 }
