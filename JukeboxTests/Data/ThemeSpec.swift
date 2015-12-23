da1c7ad2ba9d7ab8a4f3a8e6a728808d8bc59c33
Vadym Markov <markov.vadym@gmail.com>
2015-12-23 18:11:33 +0100

    Add theme specs

---
+++ JukeboxTests/Data/ThemeSpec.swift
@@ -0,0 +1,30 @@
+import Quick
+import Nimble
+@testable import Jukebox
+
+class ThemeSpec: QuickSpec {
+
+  override func spec() {
+    describe("Theme") {
+      var theme: Theme!
+
+      beforeEach {
+        theme = Theme(
+          name: "Amazing",
+          audioFormat: "aiff")
+      }
+
+      describe("#bundlePrefix") {
+        it("has correct bundle prefix") {
+          expect(Theme.bundlePrefix).to(equal("Jukebox-Sounds"))
+        }
+      }
+
+      describe(".bundleName") {
+        it("build bundle name based on prefix and theme name") {
+          expect(theme.bundleName).to(equal("\(Theme.bundlePrefix)-\(theme.name)"))
+        }
+      }
+    }
+  }
+}
