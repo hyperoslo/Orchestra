73740f249860910bb5329160a8499313f105909c
Vadym Markov <markov.vadym@gmail.com>
2016-01-18 23:46:53 +0100

    Add new sounds

---
+++ Example/JukeboxDemo/JukeboxDemo/Sources/TeamController.swift
@@ -0,0 +1,9 @@
+//
+//  TeamController.swift
+//  JukeboxDemo
+//
+//  Created by Vadym Markov on 18/01/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
---
+++ Source/Extensions/UITabBar+Jukebox.swift
@@ -0,0 +1,9 @@
+//
+//  UITabBar+Jukebox.swift
+//  Jukebox
+//
+//  Created by Vadym Markov on 18/01/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
