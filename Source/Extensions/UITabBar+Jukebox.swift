77f18b3db80ae930c8c07095fe82c4654b14cb75
Vadym Markov <markov.vadym@gmail.com>
2016-01-18 23:48:26 +0100

    UITabBar sound on select

--- Source/Extensions/UITabBar+Jukebox.swift
+++ Source/Extensions/UITabBar+Jukebox.swift
@@ -1,9 +1,29 @@
-//
-//  UITabBar+Jukebox.swift
-//  Jukebox
-//
-//  Created by Vadym Markov on 18/01/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UITabBar {
+
+  // MARK: - Method Swizzling
+
+  public override class func initialize() {
+    struct Static {
+      static var token: dispatch_once_t = 0
+    }
+
+    if self !== UITabBar.self { return }
+
+    dispatch_once(&Static.token) {
+      MethodSwizzler.swizzleMethod("setSelectedItem:", cls: self)
+    }
+  }
+
+  func jukebox_setSelectedItem(tabBarItem: UITabBarItem?) {
+    let selected = tabBarItem == selectedItem
+
+    jukebox_setSelectedItem(tabBarItem)
+
+    if selected {
+      Jukebox.autoPlay(Sound.TabBar)
+    }
+  }
+}
+
