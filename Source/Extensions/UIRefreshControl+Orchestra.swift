ebc9f0385b83dd6750a2bfe882b0a8803977dc43
Vadym Markov <markov.vadym@gmail.com>
2016-02-02 10:17:14 +0100

    Swizzle refresh control

--- Source/Extensions/UIRefreshControl+Orchestra.swift
+++ Source/Extensions/UIRefreshControl+Orchestra.swift
@@ -1,9 +1,65 @@
-//
-//  UIRefreshControl+Orchestra.swift
-//  Orchestra
-//
-//  Created by Vadym Markov on 02/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+import UIKit
+import Sugar
 
-import Foundation
+extension UIRefreshControl {
+
+  // MARK: - Method Swizzling
+
+  public override class func initialize() {
+    struct Static {
+      static var token: dispatch_once_t = 0
+    }
+
+    if self !== UIRefreshControl.self { return }
+
+    dispatch_once(&Static.token) {
+      Swizzler.swizzle("init", cls: self, prefix: "orchestra")
+      Swizzler.swizzle("initWithFrame:", cls: self, prefix: "orchestra")
+      Swizzler.swizzle("initWithCoder:", cls: self, prefix: "orchestra")
+      Swizzler.swizzle("beginRefreshing", cls: self, prefix: "orchestra")
+      Swizzler.swizzle("endRefreshing", cls: self, prefix: "orchestra")
+    }
+  }
+
+  func orchestra_init() -> UIRefreshControl {
+    let instance = orchestra_init()
+    instance.addTarget(instance, action: "playSound",
+      forControlEvents: .ValueChanged)
+
+    return instance
+  }
+
+  func orchestra_init(frame frame: CGRect) -> UIRefreshControl {
+    let instance = orchestra_init(frame: frame)
+    instance.addTarget(instance, action: "playSound",
+      forControlEvents: .ValueChanged)
+
+    return instance
+  }
+
+  func orchestra_init(coder aDecoder: NSCoder) -> UIRefreshControl {
+    let instance = orchestra_init(coder: aDecoder)
+    instance.addTarget(instance, action: "playSound",
+      forControlEvents: .ValueChanged)
+
+    return instance
+  }
+
+  func orchestra_beginRefreshing() {
+    orchestra_beginRefreshing()
+
+    Kapellmeister.engine.autoPlay(.RefreshBegin)
+  }
+
+  func orchestra_endRefreshing() {
+    orchestra_endRefreshing()
+
+    Kapellmeister.engine.autoPlay(.RefreshEnd)
+  }
+
+  // MARK: - Actions
+
+  func playSound() {
+    Kapellmeister.engine.autoPlay(.RefreshBegin)
+  }
+}
